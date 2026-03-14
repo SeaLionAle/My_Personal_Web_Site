143ca62f26a403069a4cfa2c673112f260d4e1c9
Alessio <alessio.colombo.bus@gmail.com>
2026-03-14 19:05:23 +0100

    a0.3 Started Nav and some changes. Starting to use JS

---
+++ Nav.js
@@ -0,0 +1,15 @@
+const Nav_Bar = document.getElementById("Nav_Bar");
+const Open_Nav_Bar = document.getElementById("Open_Nav_Bar");
+const Close_Nav_Bar = document.getElementById("Close_Nav_Bar");
+
+Close_Nav_Bar.addEventListener("click", () => {
+  Nav_Bar.classList.add("Hide");
+  setTimeout(() => {
+    Open_Nav_Bar.style.display = "flex";
+  }, 400);
+});
+
+Open_Nav_Bar.addEventListener("click", () => {
+  Open_Nav_Bar.style.display = "none";
+  Nav_Bar.classList.remove("Hide");
+});
